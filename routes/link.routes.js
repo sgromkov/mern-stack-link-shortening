2304b12d29a22b36d37394eff68045a692950641
Sergey Gromkov <sgromkov@gmail.com>
2021-04-06 20:23:09 +0300

    add link router

---
+++ routes/link.routes.js
@@ -0,0 +1,63 @@
+const { Router } = require('express');
+const config = require('config');
+const shortid = require('shortid');
+const Link = require('../models/Link');
+const auth = require('../middleware/auth.middleware');
+
+const router = Router();
+
+router.post('/generate', auth, async (req, res) => {
+    try {
+        const baseUrl = config.get('baseUrl');
+        const { from } = req.body;
+
+        const code = shortid.generate();
+
+        const existedLink = await Link.findOne({ from });
+
+        if (existedLink) {
+            return res.json({ link: existedLink });
+        }
+
+        const to = `${baseUrl}/t/${code}`;
+
+        const link = new Link({
+            code,
+            to,
+            from,
+            owner: req.user.userId
+        });
+
+        await link.save();
+
+        res.status(201).json({ link });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Что-то пошло не так, попробуйте снова'
+        });
+    }
+});
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const links = await Link.find({ owner: req.user.userId });
+        res.json(links);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Что-то пошло не так, попробуйте снова'
+        });
+    }
+});
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const link = await Link.findById(req.params.id);
+        res.json(link);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Что-то пошло не так, попробуйте снова'
+        });
+    }
+});
+
+module.exports = router;
