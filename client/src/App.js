794e79f1e1ea490e83af864ea5937f1a5c597324
Sergey Gromkov <sgromkov@gmail.com>
2021-04-09 15:42:47 +0300

    make DetailPAge and LinksPage

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { useAuth } from './hooks/auth.hook';
 import { AuthContext } from './context/authContext';
 import { Navbar } from './components/Navbar';
+import { Loader } from './components/Loader';
 
 function App() {
-    const { login, logout, token, userId } = useAuth();
+    const { login, logout, token, userId, ready } = useAuth();
     const isAuthenticated = !!token;
     const routes = useRoutes(isAuthenticated);
+
+    if (!ready) {
+        return <Loader />;
+    }
 
     return (
         <AuthContext.Provider value={{
--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/authContext';
+import { Loader } from '../components/Loader';
+import { LinksList } from '../components/LinksList';
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([]);
+
+    const { loading, request } = useHttp();
+
+    const { token } = useContext(AuthContext);
+
+    const fetchLinks = useCallback(async () => {
+        try {
+            const response = await request('/api/link', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            });
+
+            setLinks(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }, [token, request]);
+
+    useEffect(() => {
+        fetchLinks();
+    }, [fetchLinks]);
+
+    if (loading) {
+        return <Loader />;
+    }
+
     return (
-        <div>
-            <h1>Links Page</h1>
-        </div>
+        <>
+            {!loading && <LinksList links={links} />}
+        </>
     );
 };
